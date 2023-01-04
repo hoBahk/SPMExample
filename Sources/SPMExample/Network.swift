a4f1bb08230bf7d248106ca0ee3b844e24e20f49
hoBahk <qudgh849@gmail.com>
2023-01-04 11:38:32 +0900

    test 1-1

--- Sources/SPMExample/Network.swift
+++ Sources/SPMExample/Network.swift
@@ -1,8 +1,15 @@
 //
-//  network.swift
+//  Network.swift
 //  SPMExample
 //
 //  Created by 박병호 on 2023/01/04.
 //
 
 import Foundation
+//import Alamofire
+
+class NetworkService {
+    func network() {
+//        print(AFError.explicitlyCancelled.url?.baseURL)
+    }
+}
